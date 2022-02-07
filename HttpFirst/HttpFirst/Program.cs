2f6130874130e3dc33234b5f19e7c62984375967
kbodyan <kbodyan@gmail.com>
2022-02-07 20:08:02 +0200

    empty_project

---
+++ HttpFirst/HttpFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace HttpFirst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
